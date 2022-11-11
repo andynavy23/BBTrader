c2b3f422bf07b69e82d18133364ddf9cdd934a5d
awfu0224 <andrew.fu@nextlink.com.tw>
2022-11-11 20:10:33 +0800

    Add get config script

--- dist/config.js
+++ dist/config.js
@@ -11,10 +11,21 @@ const binance_config = {
     apiKey: process.env.BINANCE_API_KEY,
     secret: process.env.BINANCE_API_SECRET,
 };
+const google_config = {
+    doc_id: process.env.GOOGLE_SHEET_API_DOC_ID,
+    eth_sheet_id: process.env.GOOGLE_SHEET_API_ETH_SHEET_ID,
+    btc_sheet_id: process.env.GOOGLE_SHEET_API_BTC_SHEET_ID,
+    creds: {
+        private_key: process.env.GOOGLE_SHEET_API_PRIVATE_KEY,
+        client_email: process.env.GOOGLE_SHEET_API_CLIENT_EMAIL,
+    },
+};
 (() => {
     console.log('BITGET Config: ');
     console.log(bitget_config);
     console.log('BINANCE Config: ');
     console.log(binance_config);
+    console.log('GOOGLE Config: ');
+    console.log(google_config);
 })();
 //# sourceMappingURL=config.js.map
